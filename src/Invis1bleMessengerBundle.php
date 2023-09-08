a8fa4ccd8050f17ff8f5ffd0cc5a951c7caffdcb
Invis1ble <Invis1ble@users.noreply.github.com>
2023-09-08 21:46:35 +0300

    Register Message Handlers.

--- src/Invis1bleMessengerBundle.php
+++ src/Invis1bleMessengerBundle.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 
 namespace Invis1ble\MessengerBundle;
 
+use Invis1ble\MessengerBundle\DependencyInjection\RegisterMessageHandlersPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class Invis1bleMessengerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RegisterMessageHandlersPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 99999);
+    }
 }
